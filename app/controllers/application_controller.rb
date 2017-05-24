254b352e49de6e57650227a8666d1a6e94f352e7
Marek Piechocki <marek.humantechnology@gmail.com>
2017-05-24 21:36:09 +0200

    add home and default route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
 
-  before_action :ok
-
-  def ok
-    render text: params.to_s and return
-  end
-  
 end
