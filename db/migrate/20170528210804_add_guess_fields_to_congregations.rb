21008020a928e5cbeb33450f0aa75c1ee0ddcaec
mapiech <marek.humantechnology@gmail.com>
2017-06-02 22:36:18 +0200

    remove some migrations

--- db/migrate/20170528210804_add_guess_fields_to_congregations.rb
+++ db/migrate/20170528210804_add_guess_fields_to_congregations.rb
@@ -1,7 +1,7 @@
 class AddGuessFieldsToCongregations < ActiveRecord::Migration[5.1]
   def change
-    add_column :congregations, :default_ip, :string
-    add_column :congregations, :default_day, :string
+    add_column :congregations, :default_ip, :integer
+    add_column :congregations, :default_day, :integer
     add_column :congregations, :default_weekend_time, :float
   end
 end
